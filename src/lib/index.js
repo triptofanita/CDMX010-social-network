463f5f193c87ece93c88e205732d86ef641cfdf9
MiriamMVentura <miriamartv@gmail.com>
2021-02-19 14:17:49 -0600

    feat(home): add template home

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,18 @@ export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
 };
+// eventos DOM PARA IMPRIMIR RENDERIZAR EN PANTALLA
+// location.pathname
+// location.origin
+
+// history.pushState
+
+// window.onpopstate
+
+// export const routes = {
+//   '/': home,
+//   // '/signup': signup,
+//   // '/login': login,
+// };
+
+//
